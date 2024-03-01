5528ff774c7b5b86a6e08d9004be0cf9688157fb
Vi Nguyen <levinguyen170602@gmail.com>
2024-03-01 10:45:43 +0700

    add package nodemon, get all user, fix bug register

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Express } from 'express';
 import authRouter from './auth.r';
+import userRouter from './user.r'
 
 function router(app: Express) {
   app.use("/auth", authRouter);
+  app.use("/users", userRouter)
 }
 
 export default router;
+
