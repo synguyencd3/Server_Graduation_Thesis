339d89981261605607d606afcd03fa927396cc02
caoqui <qui7d18@gmail.com>
2024-03-10 20:14:28 +0700

    get and update profile user

--- src/controllers/user.c.ts
+++ src/controllers/user.c.ts
@@ -10,10 +10,10 @@ const userController = {
     },
     getUserById: async (req: Request, res: Response) => {
         const userRepository = getRepository(User);
-        const  user_id  = req.params.id; // Lấy id từ URL params
+        const user_id = req.params.id; // Lấy id từ URL params
         // console.log("user_id", user_id);
         try {
-            const userDb = await userRepository.find({where: {user_id: user_id}});
+            const userDb = await userRepository.find({ where: { user_id: user_id } });
             if (!userDb) {
                 return res.status(404).json({ message: "User not found" });
             }
@@ -25,8 +25,51 @@ const userController = {
             console.error("Error retrieving user:", error);
             return res.status(500).json({ msg: "Internal server error" });
         }
-    }
-    
+    },
+
+    getProfile: async (req: Request, res: Response) => {
+        const userRepository = getRepository(User);
+        const userId: any = req.headers['userId'] || "";
+        try {
+            const userDb = await userRepository.findOneOrFail({ where: { user_id: userId } });
+            const { password, ...others } = userDb;
+
+            return res.json({
+                status: "success",
+                profile: others
+            });
+        } catch (error) {
+            return res.json({
+                status: "failed",
+                msg: "Invalid information."
+            });
+        }
+    },
+
+    updateProfile: async (req: Request, res: Response) => {
+        const userRepository = getRepository(User);
+        const userId: any = req.headers['userId'] || "";
+        let {newProfile} = req.body;
+
+        const {user_id, username, password, google, facebook, role, aso, ...other} = newProfile;
+        
+        try {
+            const userDb = await userRepository.findOneOrFail({ where: { user_id: userId } });
+            const saveProfile = {...userDb, ...other};
+            await userRepository.save(saveProfile);
+
+            return res.json({
+                status: "success", 
+                msg: "Update successfully!"
+            })
+        } catch (error) {
+            return res.json({
+                status: "failed",
+                msg: "Invalid information."
+            });
+        }
+    },
+
 };
 
 export default userController;
--- src/routes/user.r.ts
+++ src/routes/user.r.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import userController from '../controllers/user.c';
+import middlewareController from '../middleware/middleware';
 
 const router = Router();
 
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
+
+router.get("/profile", middlewareController.verifyToken, middlewareController.getUserId, userController.getProfile);
+router.post("/profile", middlewareController.verifyToken, middlewareController.getUserId, userController.updateProfile);
 
 export default router;
