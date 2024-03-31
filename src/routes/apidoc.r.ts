89d4fc6f60abee37e242a2505490a753fd9f36c4
caoqui <qui7d18@gmail.com>
2024-03-31 15:08:21 +0700

    init microservices

--- src/routes/apidoc.r.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import apidocController from '../controllers/apidoc.c';
-
-const router = Router();
-
-router.get("/", apidocController.homePage);
-
-export default router;
--- src/routes/news.r.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import newsController from '../controllers/news.c';
-
-const router = Router();
-
-router.get("/craws", newsController.crawsNews);
-
-export default router;
