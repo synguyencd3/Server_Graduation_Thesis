2e4edda18be4caef385817fe8d325b530f172b1c
caoqui <qui7d18@gmail.com>
2024-04-09 14:18:38 +0700

    update permission admin team

--- src/controllers/admin.c.ts
+++ src/controllers/admin.c.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from "typeorm";
-import { User } from '../entities';
+import { Permission, User } from '../entities';
 import parsePermission from '../helper/parsePermission';
 
 
@@ -30,16 +30,39 @@ const adminController = {
     updatePermission: async (req: Request, res: Response) => {
         const {key, name, method} = req.body;
         const adminRepository = getRepository(User);
-        
 
         try {
-            const adminDb: User = await adminRepository.findOneOrFail({
+            // save key - name for permission.
+            let newPermission = new Permission()
+            const permissionRepository = getRepository(Permission);
+            newPermission.key = key;
+            newPermission.name = name;
+            await permissionRepository.save(newPermission);
+            let adminDb: User = await adminRepository.findOneOrFail({
                 where: { user_id: process.env.USER_ID_ADMIN_TEAM, role: "admin" }
-            })
+            });
 
+            // delete all method in permission.
+            for (let m of ["C", "R", "U", "D"])
+                adminDb.permissions = adminDb.permissions.filter((p) => p!= `${m}_${key}`);
+
+            // add new method belong to key permission
+            for (let m of method)
+                adminDb.permissions.push(`${m}_${key}`);
+                        
+            await adminRepository.save(adminDb);
+
+            return res.json({
+                status: "success",
+                msg: "Update permission sucessfully!",
+                permissions: await parsePermission(adminDb.permissions)
+            });
             
         } catch (error) {
-            
+            return res.json({
+                status: "failed",
+                msg: "error with update permisison."
+            })
         }
     }
 
--- src/helper/initData.ts
+++ src/helper/initData.ts
@@ -6,43 +6,62 @@ import { User, Permission } from "../entities";
 dotenv.config({ path: './server/.env' });
 
 export const initAdminTeam = async () => {
-    // init data
-    const userRepository = getRepository(User);
-    const user = new User();
-    const salt = await bcrypt.genSalt(11);
-    user.user_id = process.env.USER_ID_ADMIN_TEAM||"";
-    user.username = process.env.USERNAME_ADMIN_TEAM||"";
-    user.password = await bcrypt.hash(process.env.PASSWORD_ADMIN_TEAM||"", salt);
-    user.role = "admin";
+  const userRepository = getRepository(User);
 
-    // add full permission for the admin.
-    const permissionRepository = getRepository(Permission);
-    const permissionDb = await permissionRepository.find();
-    console.log("PMSDB: ", permissionDb)
-    let permissionAdmin = [];
-    for (const per of permissionDb) {
-      permissionAdmin.push(`C_${per.key}`);
-      permissionAdmin.push(`R_${per.key}`);
-      permissionAdmin.push(`U_${per.key}`);
-      permissionAdmin.push(`D_${per.key}`);
-    }
+  // check the first init
+  try {
+    await userRepository.findOneOrFail({
+      where: {user_id: process.env.USER_ID_ADMIN_TEAM}
+    })
+    
+    return;
+  } catch (error) { }
 
-    user.permissions = permissionAdmin;
+  // init data
+  const user = new User();
+  const salt = await bcrypt.genSalt(11);
+  user.user_id = process.env.USER_ID_ADMIN_TEAM || "";
+  user.username = process.env.USERNAME_ADMIN_TEAM || "";
+  user.password = await bcrypt.hash(process.env.PASSWORD_ADMIN_TEAM || "", salt);
+  user.role = "admin";
 
-    try {
-      await userRepository.save(user);
-      console.log("init admin account successfully!");
-      // init permission
-    } catch (error) { console.log("ERROR INIT DB: ", error)}
+  // add full permission for the admin.
+  const permissionRepository = getRepository(Permission);
+  const permissionDb = await permissionRepository.find();
+  let permissionAdmin = [];
+  for (const per of permissionDb) {
+    permissionAdmin.push(`C_${per.key}`);
+    permissionAdmin.push(`R_${per.key}`);
+    permissionAdmin.push(`U_${per.key}`);
+    permissionAdmin.push(`D_${per.key}`);
+  }
+
+  user.permissions = permissionAdmin;
+
+  try {
+    await userRepository.save(user);
+    console.log("init admin account successfully!");
+    // init permission
+  } catch (error) { console.log("ERROR INIT DB: ", error) }
 }
 
 export const initPermission = async (key: string, name: string) => {
-    const permissionRepository = getRepository(Permission);
-    let permission = new Permission();
-    permission.key = key;
-    permission.name = name;
-    try {
-        await permissionRepository.save(permission);
-        console.log(`init permission ${key} - ${name} successfully!`);
-      } catch (error) { console.log(`ERROR INIT permission  ${key} - ${name}: `, error)} 
+  const permissionRepository = getRepository(Permission);
+
+  // check first permission
+  try {
+    await permissionRepository.findOneOrFail({
+      where: { key: key }
+    })
+    return;
+  } catch (error) { }
+
+  let permission = new Permission();
+  permission.key = key;
+  permission.name = name;
+
+  try {
+    await permissionRepository.save(permission);
+    console.log(`init permission ${key} - ${name} successfully!`);
+  } catch (error) { console.log(`ERROR INIT permission  ${key} - ${name}: `, error) }
 }
