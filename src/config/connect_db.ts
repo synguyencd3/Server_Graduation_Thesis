1f0b9cdd46613789e28a442f70c6981158c2d708
caoqui <qui7d18@gmail.com>
2024-04-09 15:58:04 +0700

    deploy database

--- src/config/connect_db.ts
+++ src/config/connect_db.ts
@@ -7,12 +7,16 @@ dotenv.config({ path: './server/.env' });
 
 export const connectionString: DataSourceOptions = {
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOT_NAME_CLOUD,
+    port: Number(process.env.DB_PORT_CLOUD),
+    username: process.env.DB_USERNAME_CLOUD,
+    password: process.env.DB_PASSWORD_CLOUD,
+    database: process.env.DB_NAME_CLOUD,
+    url: process.env.DB_URL_CLOUD,
     entities: [User, Package, Feature, Car, Salon, Notification, Purchase, Message, Conversation, Appointment, Permission],
-    synchronize: true
+    synchronize: true,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 
